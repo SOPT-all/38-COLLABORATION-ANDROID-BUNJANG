f5568136d95aa06848597c577da1757903273de8
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-11 21:58:18 +0900

    add/#3: 디자인 시스템 폰트 preview 추가

--- app/src/main/java/com/sopt/bunjang/core/designsystem/theme/Type.kt
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/theme/Type.kt
@@ -1,10 +1,20 @@
 package com.sopt.bunjang.core.designsystem.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import com.sopt.bunjang.R
@@ -186,3 +196,40 @@ val defaultBunjangTypography = BunjangTypography(
     )
 
 )
+
+@Preview(showBackground = true)
+@Composable
+private fun BunjangTypographyPreview() {
+    BunjangTheme {
+        Column(
+            modifier = Modifier
+                .background(BunjangTheme.colors.white)
+                .padding(8.dp)
+        ) {
+            Text("Title b19", style = BunjangTheme.typography.title.b19)
+            Text("Title b18", style = BunjangTheme.typography.title.b18)
+            Text("Title b16", style = BunjangTheme.typography.title.b16)
+            Text("Title sb16", style = BunjangTheme.typography.title.sb16)
+            Text("Title b14", style = BunjangTheme.typography.title.b14)
+            Text("Title sb14", style = BunjangTheme.typography.title.sb14)
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text("Body m14", style = BunjangTheme.typography.body.m14)
+            Text("Body b14_1", style = BunjangTheme.typography.body.b14_1)
+            Text("Body sb12", style = BunjangTheme.typography.body.sb12)
+            Text("Body sb12_1", style = BunjangTheme.typography.body.sb12_1)
+            Text("Body r12", style = BunjangTheme.typography.body.r12)
+            Text("Body m11", style = BunjangTheme.typography.body.m11)
+            Text("Body m11_1", style = BunjangTheme.typography.body.m11_1)
+            Text("Body m10", style = BunjangTheme.typography.body.m10)
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text("label b11", style = BunjangTheme.typography.label.b11)
+            Text("label sb11", style = BunjangTheme.typography.label.sb11)
+            Text("label b10", style = BunjangTheme.typography.label.b10)
+            Text("label r10", style = BunjangTheme.typography.label.r10)
+        }
+    }
+}
