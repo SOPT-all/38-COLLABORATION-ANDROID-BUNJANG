094b0aaf6d09f6de0063efe378d820edf9be4307
seungjae708 <choesj365@gmail.com>
2026-05-11 18:20:20 +0900

    feat/#5: MainTab 추가

---
+++ app/src/main/java/com/sopt/bunjang/presentation/main/MainTab.kt
@@ -0,0 +1,54 @@
+package com.sopt.bunjang.presentation.main
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.sopt.bunjang.R
+import com.sopt.bunjang.core.navigation.MainTabRoute
+import com.sopt.bunjang.core.navigation.Route
+import com.sopt.bunjang.presentation.chat.navigation.Chat
+import com.sopt.bunjang.presentation.favorite.navigation.Favorite
+import com.sopt.bunjang.presentation.home.navigation.Home
+import com.sopt.bunjang.presentation.mypage.navigation.MyPage
+import com.sopt.bunjang.presentation.register.navigation.Register
+
+enum class MainTab(
+    @param:DrawableRes val icon: Int,
+    @param:StringRes val contentDescription: Int,
+    val route: MainTabRoute,
+) {
+    HOME(
+        icon = R.drawable.ic_launcher_background,
+        contentDescription = R.string.tab_home,
+        route = Home,
+    ),
+    FAVORITE(
+        icon = R.drawable.ic_launcher_background,
+        contentDescription = R.string.tab_favorite,
+        route = Favorite,
+    ),
+    REGISTER(
+        icon = R.drawable.ic_launcher_background,
+        contentDescription = R.string.tab_register,
+        route = Register,
+    ),
+    CHAT(
+        icon = R.drawable.ic_launcher_background,
+        contentDescription = R.string.tab_chat,
+        route = Chat,
+    ),
+    MY_PAGE(
+        icon = R.drawable.ic_launcher_background,
+        contentDescription = R.string.tab_my_page,
+        route = MyPage,
+    );
+
+    companion object {
+        fun find(predicate: (MainTabRoute) -> Boolean): MainTab? {
+            return entries.find { predicate(it.route) }
+        }
+
+        fun contains(predicate: (Route) -> Boolean): Boolean {
+            return entries.map { it.route }.any { predicate(it) }
+        }
+    }
+}
