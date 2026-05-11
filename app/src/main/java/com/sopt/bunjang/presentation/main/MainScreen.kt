b441f329fb2156a4547876a8823b256f577b656b
seungjae708 <choesj365@gmail.com>
2026-05-11 18:20:56 +0900

    feat/#5: MainScreen 구현

---
+++ app/src/main/java/com/sopt/bunjang/presentation/main/MainScreen.kt
@@ -0,0 +1,104 @@
+package com.sopt.bunjang.presentation.main
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.compose.NavHost
+import com.sopt.bunjang.presentation.chat.navigation.chatGraph
+import com.sopt.bunjang.presentation.dummy.navigation.dummyGraph
+import com.sopt.bunjang.presentation.favorite.navigation.favoriteGraph
+import com.sopt.bunjang.presentation.home.navigation.homeGraph
+import com.sopt.bunjang.presentation.main.component.MainBottomBar
+import com.sopt.bunjang.presentation.mypage.navigation.myPageGraph
+import com.sopt.bunjang.presentation.register.navigation.registerGraph
+import kotlinx.collections.immutable.toPersistentList
+
+@Composable
+fun MainScreen(
+    appState: MainAppState = rememberMainAppState(),
+) {
+    val isBottomBarVisible by appState.isBottomBarVisible.collectAsStateWithLifecycle()
+    val currentTab by appState.currentTab.collectAsStateWithLifecycle()
+
+    Scaffold(
+        bottomBar = {
+            MainBottomBar(
+                isVisible = isBottomBarVisible,
+                tabs = MainTab.entries.toPersistentList(),
+                currentTab = currentTab,
+                onTabSelected = appState::navigate
+            )
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .statusBarsPadding()
+    ) { innerPadding ->
+        NavHost(
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> fullWidth },
+                    animationSpec = tween(durationMillis = 300)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> -fullWidth },
+                    animationSpec = tween(durationMillis = 300)
+                )
+            },
+            popEnterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> -fullWidth },
+                    animationSpec = tween(durationMillis = 300)
+                )
+            },
+            popExitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> fullWidth },
+                    animationSpec = tween(durationMillis = 300)
+                )
+            },
+            navController = appState.navController,
+            startDestination = appState.startDestination
+        ) {
+            dummyGraph(
+                paddingValues = innerPadding,
+                navigateUp = appState::navigateUp
+            )
+
+            homeGraph(
+                paddingValues = innerPadding,
+                navigateUp = appState::navigateUp
+            )
+
+            favoriteGraph(
+                paddingValues = innerPadding,
+                navigateUp = appState::navigateUp
+            )
+
+            registerGraph(
+                paddingValues = innerPadding,
+                navigateUp = appState::navigateUp
+            )
+
+            chatGraph(
+                paddingValues = innerPadding,
+                navigateUp = appState::navigateUp
+            )
+
+            myPageGraph(
+                paddingValues = innerPadding,
+                navigateUp = appState::navigateUp
+            )
+        }
+    }
+}
