e86b2ecff9b985629da33ac5c86d5e134f094dbf
seungjae708 <choesj365@gmail.com>
2026-05-11 18:20:31 +0900

    feat/#5: MainBottomBar 구현

---
+++ app/src/main/java/com/sopt/bunjang/presentation/main/component/MainBottomBar.kt
@@ -0,0 +1,108 @@
+package com.sopt.bunjang.presentation.main.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.bunjang.core.extension.noRippleClickable
+import com.sopt.bunjang.presentation.main.MainTab
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
+
+@Composable
+fun MainBottomBar(
+    isVisible: Boolean,
+    tabs: ImmutableList<MainTab>,
+    currentTab: MainTab?,
+    onTabSelected: (MainTab) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = slideInVertically(initialOffsetY = { it }) + fadeIn(),
+        exit = slideOutVertically(targetOffsetY = { it }) + fadeOut(),
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(28.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            tabs.forEach { tab ->
+                MainBottomBarTab(
+                    tab = tab,
+                    isSelected = tab == currentTab,
+                    onClick = { onTabSelected(tab) },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+// Todo: 스타일 및 색상 변경 필요
+@Composable
+fun MainBottomBarTab(
+    tab: MainTab,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier
+            .noRippleClickable(onClick)
+            .padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(tab.icon),
+            contentDescription = tab.contentDescription.toString(),
+            tint = if (isSelected) Color.Black
+            else Color.Gray,
+            modifier = Modifier.size(24.dp)
+        )
+
+        Spacer(modifier = Modifier.height(2.dp))
+
+        Text(
+            text = stringResource(tab.contentDescription),
+            style = MaterialTheme.typography.labelSmall,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MainBottomBarPreview() {
+   MainBottomBar(
+        isVisible = true,
+        tabs = MainTab.entries.toImmutableList(),
+        currentTab = MainTab.entries.first(),
+        onTabSelected = {}
+    )
+}
