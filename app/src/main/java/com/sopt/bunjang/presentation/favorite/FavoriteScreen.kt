e1167a428df5fb8a0892465dabdc448d91b2ee7f
seungjae708 <choesj365@gmail.com>
2026-05-11 18:23:12 +0900

    feat/#5: FavoriteScreen, FavoriteNavigation 추가

---
+++ app/src/main/java/com/sopt/bunjang/presentation/favorite/FavoriteScreen.kt
@@ -0,0 +1,20 @@
+package com.sopt.bunjang.presentation.favorite
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoriteRoute() {
+    FavoriteScreen()
+}
+
+@Composable
+private fun FavoriteScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text(text = "Favorite Screen")
+    }
+}
