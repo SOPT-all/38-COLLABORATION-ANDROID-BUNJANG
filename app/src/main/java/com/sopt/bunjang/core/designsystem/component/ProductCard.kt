84965226f90920cd9c68d14e9295d4a91db2aa5f
apffxhsls <gyeji975@gmail.com>
2026-05-12 22:44:04 +0900

    chore/#1: 사용 안 하는 import 제거

--- app/src/main/java/com/sopt/bunjang/core/designsystem/component/ProductCard.kt
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/component/ProductCard.kt
@@ -3,7 +3,6 @@ package com.sopt.bunjang.core.designsystem.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +14,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
