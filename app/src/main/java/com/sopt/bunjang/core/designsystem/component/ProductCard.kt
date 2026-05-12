f478bbf23621f0d80e6b8c8793c5af08716779fa
apffxhsls <gyeji975@gmail.com>
2026-05-12 22:21:40 +0900

    feat/#1: BigProductCard 구현

--- app/src/main/java/com/sopt/bunjang/core/designsystem/component/ProductCard.kt
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/component/ProductCard.kt
@@ -1,14 +1,19 @@
 package com.sopt.bunjang.core.designsystem.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -21,10 +26,72 @@ data class ProductCardState(
     val title: String,
     val createdAt: String? = null,
     val isFavorite: Boolean = false,
-    val favoriteCount: Int? = null,
+    val favoriteCount: Int? = 0,
 ) {
     val formattedPrice: String
         get() = "%,d".format(price) + "원"
+}
+
+@Composable
+fun BigProductCard(
+    state: ProductCardState,
+    modifier: Modifier = Modifier,
+    onCardClick: () -> Unit = {},
+    onFavoriteClick: () -> Unit = {}
+) {
+    Column(
+        modifier = modifier
+            .size(width = 160.dp, height = 266.dp)
+            .background(BunjangTheme.colors.white)
+            .clickable { onCardClick() }
+    ) {
+        AsyncImage(
+            model = state.imageUrl,
+            contentDescription = state.title,
+            modifier = Modifier
+                .size(width = 160.dp, height = 194.dp)
+                .clip(RoundedCornerShape(4.dp))
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            // 가격
+            text = state.formattedPrice,
+            style = BunjangTheme.typography.title.title5,
+            color = BunjangTheme.colors.gray900
+        )
+
+        Text(
+            // 상품명
+            text = state.title,
+            style = BunjangTheme.typography.body.body2,
+            color = BunjangTheme.colors.gray700
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        if (state.createdAt != null) {
+            Text(
+                text = state.createdAt,
+                style = BunjangTheme.typography.label.label2,
+                color = BunjangTheme.colors.gray400
+            )
+        }
+        Box(modifier = Modifier.fillMaxWidth()) {
+            if (state.favoriteCount != null) {
+                Text(
+                    text = state.favoriteCount.toString(),
+                    style = BunjangTheme.typography.label.label2,
+                    color = BunjangTheme.colors.gray500,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(horizontal = 2.dp, vertical = 8.dp)
+                )
+            }
+            // icon 넣기
+        }
+    }
+
 }
 
 @Composable
@@ -35,33 +102,33 @@ fun SmallProductCard(
 ) {
     Column(
         modifier = modifier
+            .background(BunjangTheme.colors.white)
             .clickable { onCardClick() }
             .width(102.dp)
-    ) {
-        Box {
-            AsyncImage(
-                model = state.imageUrl,
-                contentDescription = state.title,
-                modifier = Modifier
-                    .height(125.dp)
-                    .clip(RoundedCornerShape(4.dp))
-            )
+    )
+    {
+        AsyncImage(
+            model = state.imageUrl,
+            contentDescription = state.title,
+            modifier = Modifier
+                .height(125.dp)
+                .clip(RoundedCornerShape(4.dp))
+        )
 
-            Spacer(modifier = Modifier.height(6.dp))
+        Spacer(modifier = Modifier.height(6.dp))
 
-            Text(
-                // 가격
-                text = state.formattedPrice,
-                style = BunjangTheme.typography.body.body1_1,
-                color = BunjangTheme.colors.gray900
-            )
-            Text(
-                // 상품명
-                text = state.title,
-                style = BunjangTheme.typography.label.label1,
-                color = BunjangTheme.colors.gray800
-            )
-            // 아이콘 띄우기
-        }
+        Text(
+            // 가격
+            text = state.formattedPrice,
+            style = BunjangTheme.typography.body.body1_1,
+            color = BunjangTheme.colors.gray900
+        )
+        Text(
+            // 상품명
+            text = state.title,
+            style = BunjangTheme.typography.label.label1,
+            color = BunjangTheme.colors.gray800
+        )
+        // 아이콘 띄우기
     }
 }
