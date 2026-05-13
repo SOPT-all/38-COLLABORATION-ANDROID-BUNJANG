6a5d52ea532b052a20a1e392f705e53d46b6c27c
seungjae708 <choesj365@gmail.com>
2026-05-13 16:55:55 +0900

    feat/#10: PurChaseScreen 추가

---
+++ app/src/main/java/com/sopt/bunjang/presentation/purchase/PurChaseScreen.kt
@@ -0,0 +1,19 @@
+package com.sopt.bunjang.presentation.purchase
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PurChaseRoute(
+    paddingValues: PaddingValues,
+    navigateUp: () -> Unit
+) {
+    PurChaseScreen()
+}
+
+@Composable
+private fun PurChaseScreen(
+
+) {
+
+}
