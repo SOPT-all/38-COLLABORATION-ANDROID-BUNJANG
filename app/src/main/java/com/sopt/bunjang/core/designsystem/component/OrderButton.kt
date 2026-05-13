8c979f568c799ac864b0cc940acde79a942b0a9d
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-13 19:03:34 +0900

    feat/#12: OrderButton 구현

---
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/component/OrderButton.kt
@@ -0,0 +1,56 @@
+package com.sopt.bunjang.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
+
+@Composable
+fun OrderButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    textColor: Color = BunjangTheme.colors.white,
+    backgroundColor: Color = BunjangTheme.colors.primaryRed
+) {
+    Row(
+        modifier = modifier
+            .width(148.dp)
+            .height(48.dp)
+            .clip(shape = RoundedCornerShape(6.dp))
+            .background(color = backgroundColor)
+            .clickable(onClick = onClick)
+            .padding(vertical = 12.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = text,
+            color = textColor,
+            style = BunjangTheme.typography.title.title3
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun OrderPreview() {
+    BunjangTheme {
+        OrderButton(
+            text = "구매하기"
+        )
+    }
+}
