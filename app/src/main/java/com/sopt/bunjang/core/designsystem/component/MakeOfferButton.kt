fb4b1b070fb73a2217c222299a0376bb8ec2d6f2
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-13 19:04:03 +0900

    feat/#12: MakeOfferButton 구현

---
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/component/MakeOfferButton.kt
@@ -0,0 +1,58 @@
+package com.sopt.bunjang.core.designsystem.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
+
+@Composable
+fun MakeOfferButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    textColor: Color = BunjangTheme.colors.black,
+    backgroundColor: Color = BunjangTheme.colors.white
+) {
+    Row(
+        modifier = modifier
+            .width(322.dp)
+            .height(38.dp)
+            .clip(shape = RoundedCornerShape(6.dp))
+            .border(1.dp, BunjangTheme.colors.gray200, RoundedCornerShape(6.dp))
+            .background(color = backgroundColor)
+            .clickable(onClick = onClick)
+            .padding(vertical = 10.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            text = text,
+            color = textColor,
+            style = BunjangTheme.typography.body.body2
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MakeOfferPreview() {
+    BunjangTheme {
+        MakeOfferButton(
+            text = "가격제안"
+        )
+    }
+}
