8e468ad438852ee95c2d8f32da6813ab7dd35aa2
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-14 20:45:17 +0900

    del/#12: 불필요한 import 제거

--- app/src/main/java/com/sopt/bunjang/core/designsystem/component/MakeOfferButton.kt
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/component/MakeOfferButton.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
-import com.sopt.bunjang.core.extension.noRippleClickable
 
 @Composable
 fun MakeOfferButton(
--- app/src/main/java/com/sopt/bunjang/core/designsystem/component/ViewAllButton.kt
+++ app/src/main/java/com/sopt/bunjang/core/designsystem/component/ViewAllButton.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
-import com.sopt.bunjang.core.extension.noRippleClickable
 
 @Composable
 fun ViewAllButton(
