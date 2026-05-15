cd94706856dc1462ab5ed07fa146883e2af877c2
apffxhsls <gyeji975@gmail.com>
2026-05-15 22:55:51 +0900

    mod/#16: HomeRecentProduct 하드코딩 코드 변경

--- app/src/main/java/com/sopt/bunjang/presentation/home/component/HomeRecentProduct.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/home/component/HomeRecentProduct.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -51,7 +53,11 @@ fun HomeRecentProduct(
     homeRecentProductList: List<HomeSimilarProduct>
 ) {
     Column(modifier = modifier) {
-        Row(modifier = modifier) {
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 17.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Text(
                 text = "최근 본 상품과 비슷한 상품",
                 style = BunjangTheme.typography.title.title3,
