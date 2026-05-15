f46c1c9abce331126271534cdb53e859c2add5a9
apffxhsls <gyeji975@gmail.com>
2026-05-15 14:24:53 +0900

    fix/#16: PreviewHomeGlassesSection icon tint 제거

--- app/src/main/java/com/sopt/bunjang/presentation/home/component/HomeGlassesSection.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/home/component/HomeGlassesSection.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -108,7 +109,7 @@ fun HomeGlassesSection(
             Icon(
                 painter = painterResource(id = R.drawable.ic_more_18),
                 contentDescription = null,
-                tint = BunjangTheme.colors.white,
+                tint = Color.Unspecified,
                 modifier = Modifier.size(18.dp)
             )
         }
