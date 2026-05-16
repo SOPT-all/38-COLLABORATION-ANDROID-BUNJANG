40c3fed1d416b985b0e04ea4ed2660cae1305ae3
apffxhsls <gyeji975@gmail.com>
2026-05-17 00:55:42 +0900

    mod/#16: HomeGlassesProduct에 @Immutable 어노테이션 추가

--- app/src/main/java/com/sopt/bunjang/presentation/home/component/HomeGlassesSection.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/home/component/HomeGlassesSection.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,6 +28,7 @@ import com.sopt.bunjang.core.designsystem.component.ProductCard
 import com.sopt.bunjang.core.designsystem.component.ProductCardType
 import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
 
+@Immutable
 data class HomeGlassesProduct(
     val imageUrl: String,
     val price: Int,
