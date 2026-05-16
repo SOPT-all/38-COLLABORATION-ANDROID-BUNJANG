d7236f08d501d4c989ee15cd959efec62bee2fbc
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-16 23:04:28 +0900

    feat/#19: 상세정보 탭 ui 구현

---
+++ app/src/main/java/com/sopt/bunjang/presentation/productdetail/component/ProductDetailTabRow.kt
@@ -0,0 +1,68 @@
+package com.sopt.bunjang.presentation.productdetail.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
+
+@Composable
+fun ProductDetailTabRow(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 19.dp)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(
+                text = "상세정보",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(top = 12.dp, bottom = 11.dp),
+                textAlign = TextAlign.Center,
+                style = BunjangTheme.typography.title.title5,
+                color = BunjangTheme.colors.gray900
+            )
+
+            Text(
+                text = "상점정보/후기",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(top = 12.dp, bottom = 11.dp),
+                textAlign = TextAlign.Center,
+                style = BunjangTheme.typography.title.title6,
+                color = BunjangTheme.colors.gray800
+            )
+        }
+
+        HorizontalDivider(
+            color = BunjangTheme.colors.gray900,
+            thickness = 3.dp,
+            modifier = Modifier
+                .width(161.dp)
+                .clip(RoundedCornerShape(100.dp))
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 360)
+@Composable
+private fun ProductDetailTabRowPreview() {
+    BunjangTheme {
+        ProductDetailTabRow()
+    }
+}
