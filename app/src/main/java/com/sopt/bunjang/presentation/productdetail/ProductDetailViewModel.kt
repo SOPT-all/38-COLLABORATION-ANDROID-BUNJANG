6bd23d7e6bfff70a8208332bbba561d1e37e2cb9
seungjae708 <choesj365@gmail.com>
2026-05-16 15:52:28 +0900

    feat/#18: ProductDetailViewModel 추가

--- app/src/main/java/com/sopt/bunjang/presentation/productdetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/productdetail/ProductDetailViewModel.kt
@@ -3,30 +3,57 @@ package com.sopt.bunjang.presentation.productdetail
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.bunjang.presentation.productdetail.state.ProductDetailSideEffect
+import com.sopt.bunjang.presentation.productdetail.state.ProductDetailUiState
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class ProductDetailViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(ProductDetailUiState.dummy)
+    val uiState: StateFlow<ProductDetailUiState> = _uiState.asStateFlow()
+
     private val _sideEffect = MutableSharedFlow<ProductDetailSideEffect>()
     val sideEffect: SharedFlow<ProductDetailSideEffect> = _sideEffect.asSharedFlow()
 
     fun onBackIconClick() {
-        viewModelScope.launch {
-            _sideEffect.emit(ProductDetailSideEffect.NavigateUp)
-        }
+        viewModelScope.launch { _sideEffect.emit(ProductDetailSideEffect.NavigateUp) }
     }
 
     fun onHomeIconClick() {
-        viewModelScope.launch {
-            _sideEffect.emit(ProductDetailSideEffect.NavigateToHome)
-        }
+        viewModelScope.launch { _sideEffect.emit(ProductDetailSideEffect.NavigateToHome) }
     }
 
     fun onPurchaseIconClick() {
-        viewModelScope.launch {
-            _sideEffect.emit(ProductDetailSideEffect.NavigateToPurchase)
+        viewModelScope.launch { _sideEffect.emit(ProductDetailSideEffect.NavigateToPurchase) }
+    }
+
+    fun onProductClick(id: Long) {
+        viewModelScope.launch { _sideEffect.emit(ProductDetailSideEffect.NavigateToProductDetail(id)) }
+    }
+
+    fun onLikeClick(id: Long) {
+        _uiState.update { state ->
+            state.copy(
+                recommendProducts = state.recommendProducts.map { product ->
+                    if (product.id == id) product.copy(isLike = !product.isLike)
+                    else product
+                }.toImmutableList(),
+
+                similarProducts = state.similarProducts.map { group ->
+                    group.copy(
+                        products = group.products.map { product ->
+                            if (product.id == id) product.copy(isLike = !product.isLike)
+                            else product
+                        }.toImmutableList()
+                    )
+                }.toImmutableList()
+            )
         }
     }
 }
