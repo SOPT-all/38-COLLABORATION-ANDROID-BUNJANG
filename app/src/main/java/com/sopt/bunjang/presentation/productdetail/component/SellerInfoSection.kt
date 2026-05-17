4c96ec35c2ffc399cbd56d7cb59c1e611f4b1b0a
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-17 15:09:44 +0900

    del/#19: 불필요한 import 제거

--- app/src/main/java/com/sopt/bunjang/presentation/productdetail/component/SellerInfoSection.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/productdetail/component/SellerInfoSection.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
