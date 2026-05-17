f13e89ef512e94eb6ef6141c5417d444eea4478f
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-17 15:06:31 +0900

    feat/#19: 상점 ProductCard 구현

---
+++ app/src/main/java/com/sopt/bunjang/presentation/productdetail/component/StoreProductCard.kt
@@ -0,0 +1,103 @@
+package com.sopt.bunjang.presentation.productdetail.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.sopt.bunjang.R
+import com.sopt.bunjang.core.designsystem.theme.BunjangTheme
+
+@Composable
+fun StoreProductCard(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+    title: String,
+    price: Int,
+    likes: Int? = null,
+) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+
+        ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = null,
+            placeholder = ColorPainter(Color.LightGray),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f / 1f)
+                .clip(RoundedCornerShape(4.dp))
+        )
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Text(
+            text = title,
+            color = BunjangTheme.colors.gray600,
+            style = BunjangTheme.typography.label.label4
+        )
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Text(
+            text = "%,d".format(price) + "원",
+            color = BunjangTheme.colors.gray700,
+            style = BunjangTheme.typography.label.label1
+        )
+
+        Spacer(modifier = Modifier.height(6.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            likes?.let { count ->
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_detail_page_heart),
+                    contentDescription = "likes",
+                    tint = BunjangTheme.colors.gray300,
+                    modifier = Modifier.size(12.dp)
+                )
+
+                Spacer(modifier = Modifier.width(1.dp))
+
+                Text(
+                    text = count.toString(),
+                    style = BunjangTheme.typography.label.label2,
+                    color = BunjangTheme.colors.gray300
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun StoreProductCardPreview() {
+    BunjangTheme {
+        StoreProductCard(
+            imageUrl = "",
+            title = "상품명",
+            price = 100000,
+            likes = 0,
+            modifier = Modifier.width(102.dp)
+        )
+    }
+}
