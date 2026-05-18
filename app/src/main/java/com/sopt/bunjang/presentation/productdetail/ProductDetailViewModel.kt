a7138614af09004f4dde11baace9129681b4e79c
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-18 23:57:10 +0900

    feat/#15: 하트 아이콘 클릭시 변화 구현

--- app/src/main/java/com/sopt/bunjang/presentation/productdetail/ProductDetailViewModel.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/productdetail/ProductDetailViewModel.kt
@@ -2,15 +2,24 @@ package com.sopt.bunjang.presentation.productdetail
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.sopt.bunjang.presentation.home.state.HomeUiState
+import com.sopt.bunjang.presentation.productdetail.component.ProductDetailCardUiModel
 import com.sopt.bunjang.presentation.productdetail.state.ProductDetailSideEffect
+import com.sopt.bunjang.presentation.productdetail.state.ProductDetailUiState
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class ProductDetailViewModel : ViewModel() {
     private val _sideEffect = MutableSharedFlow<ProductDetailSideEffect>()
     val sideEffect: SharedFlow<ProductDetailSideEffect> = _sideEffect.asSharedFlow()
+
+    private val _uiState = MutableStateFlow(ProductDetailUiState())
+    val uiState: StateFlow<ProductDetailUiState> = _uiState.asStateFlow()
 
     fun onBackIconClick() {
         viewModelScope.launch {
--- app/src/main/java/com/sopt/bunjang/presentation/productdetail/state/ProductDetailContract.kt
+++ app/src/main/java/com/sopt/bunjang/presentation/productdetail/state/ProductDetailContract.kt
@@ -1,5 +1,12 @@
 package com.sopt.bunjang.presentation.productdetail.state
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class ProductDetailUiState(
+    val isLike: Boolean = false
+)
+
 sealed interface ProductDetailSideEffect {
     data object NavigateUp : ProductDetailSideEffect
     data object NavigateToHome : ProductDetailSideEffect
