f90e557a74a9fb8ff93b7ddcc62f169def39bcdc
jiyoung2ee <mmuscat33@dgu.ac.kr>
2026-05-19 21:41:01 +0900

    del/#27: 불필요한 import 제거

--- app/src/main/java/com/sopt/bunjang/data/productdetail/remote/datasource/ProductDetailDataSource.kt
+++ app/src/main/java/com/sopt/bunjang/data/productdetail/remote/datasource/ProductDetailDataSource.kt
@@ -1,7 +1,6 @@
 package com.sopt.bunjang.data.productdetail.remote.datasource
 
 import com.sopt.bunjang.data.productdetail.remote.dto.response.ProductDetailResponseDto
-import retrofit2.Response
 
 interface ProductDetailDataSource {
     suspend fun getProductDetail(
